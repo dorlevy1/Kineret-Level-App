c3d51d6ff2a35e19fee284c23fa4cf2cdd0edc32
dor <ddl105095@gmail.com>
2020-08-28 04:35:47 +0300

    adding build(production) folder V.0.0.6

---
+++ build/precache-manifest.2745451509fab5aa6e54b473d59d8f3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635203de81307f7e2f280c9697c0b640",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06ea1839a861dd777505",
+    "url": "/static/css/2.42be0312.chunk.css"
+  },
+  {
+    "revision": "e6fb2791b4fa4d59c227",
+    "url": "/static/css/main.80705c13.chunk.css"
+  },
+  {
+    "revision": "06ea1839a861dd777505",
+    "url": "/static/js/2.533a29c8.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/static/js/2.533a29c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6fb2791b4fa4d59c227",
+    "url": "/static/js/main.3493c847.chunk.js"
+  },
+  {
+    "revision": "ffcabf28e979e045799d",
+    "url": "/static/js/runtime-main.85b2a653.js"
+  },
+  {
+    "revision": "b5cb9826f12b59b43ab2573fc1c6c5c0",
+    "url": "/static/media/data.b5cb9826.csv"
+  }
+]);

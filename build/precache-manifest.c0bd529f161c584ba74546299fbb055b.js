62fef4e86ad5fb151a66d50c00e3bf8feee5b714
Dor Dylan Levy <ddl105095@gmail.com>
2020-09-10 22:34:32 +0300

    changes

---
+++ build/precache-manifest.c0bd529f161c584ba74546299fbb055b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476b0f6370fc0108880a0118d6db760b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e66c3cb7c621bdfa1eae",
+    "url": "/static/css/main.597e3102.chunk.css"
+  },
+  {
+    "revision": "98879588adf939ef0f90",
+    "url": "/static/js/2.c2c4d3a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c2c4d3a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e66c3cb7c621bdfa1eae",
+    "url": "/static/js/main.887ff0e5.chunk.js"
+  },
+  {
+    "revision": "ffcabf28e979e045799d",
+    "url": "/static/js/runtime-main.85b2a653.js"
+  },
+  {
+    "revision": "bedf7d34426c25d4d05e2e8482748ef6",
+    "url": "/static/media/bgc_start.bedf7d34.png"
+  },
+  {
+    "revision": "b2e8c6bb7c7c6b888e02c02a4669307a",
+    "url": "/static/media/logos.b2e8c6bb.png"
+  }
+]);
